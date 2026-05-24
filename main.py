4dab99c8986e0ae9484dfe22f97615190bb03d4f
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-05-24 20:13:14 -0300

    Implementa rotas e endpoints com Request Body em FastAPI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import UTC, datetime
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,10 +11,21 @@ fake_db = [
     {"title": f"Criando uma aplicação com Starlett", "date": datetime.now(UTC),"published": False },
 ]
 
+class Post(BaseModel):
+    title: str
+    date: datetime = datetime.now(UTC)
+    published: bool = False
+    
+
+
+@app.post('/post/', status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    fake_db.append(post.model_dump())
+    return post
 
 @app.get("/posts")
-def read_posts(  published: bool, skip: int = 0, limit: int = len(fake_db)):
-    return [post for post in fake_db[skip : skip + limit] if post ["published"] is published]
+def read_posts(  published: bool, limit: int, skip: int = 0):
+    return [post for post in fake_db[skip : skip + limit] if post["published"] is published]
 
 
 @app.get("/posts/{framework}")
