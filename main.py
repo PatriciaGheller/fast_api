22ac7d14bee771910041a1cbeca717891412a6ab
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-05-24 18:29:57 -0300

    Implementa rotas e endpoints com Query Parameters em FastAPI

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_db = [
+    {"title": f"Criando uma aplicação com FastAPI", "date": datetime.now(UTC),"published": True },
+    {"title": f"Criando uma aplicação com Flask", "date": datetime.now(UTC),"published": True}, 
+    {"title": f"Criando uma aplicação com Django", "date": datetime.now(UTC),"published": True}, 
+    {"title": f"Criando uma aplicação com Starlett", "date": datetime.now(UTC),"published": False },
+]
+
+
+@app.get("/posts")
+def read_posts(  published: bool, skip: int = 0, limit: int = len(fake_db)):
+    return [post for post in fake_db[skip : skip + limit] if post ["published"] is published]
+
 
 @app.get("/posts/{framework}")
-def read_posts(framework: str):
+def read_framework_posts(framework: str):
     return {
         "posts": [
-            {'title': f'Criando uma aplicação com {framework}', 'date': datetime.now(UTC)}, 
-            {'title': f'Internacionalizando uma app {framework}', 'date': datetime.now(UTC)}
+            {"title": f"Criando uma aplicação com {framework}", "date": datetime.now(UTC)}, 
+            {"title": f"Criando uma aplicação com {framework}", "date": datetime.now(UTC)}
         ]
     }
 
