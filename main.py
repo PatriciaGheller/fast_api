f76ccc3bc09884963beaef7b2c46a88f2d5f52c4
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-05-25 20:20:51 -0300

    Configura conexão assíncrona ao banco de dados no FastAPI

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+from contextlib import asynccontextmanager
+import databases
+import sqlalchemy as sa
+from controllers import post
 from fastapi import FastAPI
 
-app = FastAPI()
+
+DATABASE_URL = "sqlite:///./blog.db"
+
+database = databases.Database(DATABASE_URL)
+metadata = sa.MetaData()
+engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+metadata.create_all(engine)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.connect()
+    
+    
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(post.router)
+
+
 
