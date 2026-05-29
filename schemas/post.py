8d649ac7a8b5a280db6be508c97d4bd9dc801bf7
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-05-29 14:50:10 -0300

    Define schemas Pydantic para posts

--- schemas/post.py
+++ schemas/post.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
-from datetime import datetime, UTC
+from datetime import datetime
 
 class PostIn(BaseModel):
     title: str
-    date: datetime = datetime.now(UTC)
+    content: str
+    published_at: datetime | None = None
     published: bool = False
