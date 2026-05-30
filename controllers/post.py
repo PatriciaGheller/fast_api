d8ce8427008994d5dd0e7471d27d9a0989047c58
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-05-30 18:11:34 -0300

    Finaliza implementação CRUD assíncrono nos controllers de posts

--- controllers/post.py
+++ controllers/post.py
@@ -7,22 +7,30 @@ from database import database
 
 router = APIRouter(prefix="/posts")
 
+service = PostService()
+
 @router.get("/", response_model=list[PostOut])
 async def read_posts(published: bool, limit: int, skip: int = 0):
-    query = posts.select()
-    return await database.fetch_all(query)
+    return await service.read_all(published=published, limit=limit, skip=skip)
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=PostOut)
 async def create_post(post: PostIn):
-    command = posts.insert().values(
-        title=post.title, 
-        content=post.content, 
-        published_at=post.published_at, 
-        published=post.published,
-    )
-    last_id = await database.execute(command)
-    return {**post.model_dump(), 'id': last_id}
+    return {**post.model_dump(), 'id': await service.create(post)}
+
+@router.get("/{id}", response_model=PostOut)
+async def read_post(id: int):
+    return await service.read(id)
+
+@router.patch("/{id}", response_model=PostOut)
+async def update_post(id: int, post: PostUpdateIn):
+    return await service.update(id=id, post=post)
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT, response_model=None)
+async def delete_post(id: int):
+    await service.delete(id)
+
+
 
 
 
