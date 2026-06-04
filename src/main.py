0185c4504884c20a7d9bf0043f5c8188c80eb303
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-06-04 18:28:34 -0300

    docs: configura FastAPI para gerar documentação automática

--- src/main.py
+++ src/main.py
@@ -17,10 +17,50 @@ async def lifespan(app: FastAPI):
     await database.disconnect()
     
     
+tags_metadata = [
+    {
+        "name": "auth",
+        "description": "Operações para autenticação.",
+    },
+    {
+        "name": "Post",
+        "description": "Operações para manter posts.",
+        "externalDocs": {
+            "description": "Documentação externa para Posts.api",
+            "url": "https://post-api.com/",
+        },
+    },
+]
 
-app = FastAPI(lifespan=lifespan)
-app.include_router(auth.router)
-app.include_router(post.router)
+
+servers = [
+    {"url": "http://localhost:8000", "description": "Servidor local"},
+    {"url": "https://api.dio-blog.com", "description": "Servidor de produção"},
+]   
+    
+
+app = FastAPI(
+    title="Dio Blog API",
+    version="1.2.0",
+    summary="API para um blog pessoal",
+    description="""API de um blog simples feita com FastAPI.
+    ## Posts
+    Você será capaz de fazer:
+    * **Criar** posts** 
+    * **Recuperar posts** 
+    * **Recuperar posts po ID** 
+    * **Atualizar posts** 
+    * **Excluir posts**  
+    * **Limitar quantidade de posts diários**
+    """,
+    openapi_tags=tags_metadata,
+    servers=servers,
+    redoc_url=None, 
+    # openapi_url= None, # desabilita a documentação automática
+    lifespan=lifespan,
+)
+app.include_router(auth.router, tags=["auth"])
+app.include_router(post.router, tags=["Posts"])
 
 @app.exception_handler(NotFoundPostError)
 async def not_found_post_exception_handler(request: Request, exc: NotFoundPostError):
