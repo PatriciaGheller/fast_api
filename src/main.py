87826f0ccecc5dd1a8f33bd0f41f43d97d5bad07
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-06-04 21:19:02 -0300

    Correção: aquisição de conexão com o banco de dados

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Request 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 from src.controllers import auth, post
+from src.database import database
 from src.exceptions import NotFoundPostError
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+    
+    
 
 tags_metadata = [
     {
@@ -45,6 +57,7 @@ app = FastAPI(
     servers=servers,
     redoc_url=None, 
     # openapi_url= None, # desabilita a documentação automática
+    lifespan=lifespan,
 )
 
 app.add_middleware(
